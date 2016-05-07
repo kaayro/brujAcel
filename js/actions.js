58736986ad447270738d67989c47aeb618c042dc
Carlos Celma <solrac@igitsoft.com>
2016-05-07 11:31:05 -0500

    a

---
+++ js/actions.js
@@ -0,0 +1,16 @@
+//actions
+var jQT = new $.jQTouch({
+	themeSelectionSelector: '#jqt'
+});
+
+var fn = {
+	ready: function(){
+		document.addEventListener("deviceready", fn.init, false);
+	}
+	init: function(){
+		$('#acelerometro .individual li').eq(0).tap(ac.iniciar);
+		$('#acelerometro .individual li').eq(1).tap(ac.detener);
+	}
+};
+
+$(fn.ready);
