27578badbf0c92a4e29fec19562d8d2abe37e8b0
Carlos Celma <solrac@igitsoft.com>
2016-05-07 11:36:57 -0500

    d

--- js/actions.js
+++ js/actions.js
@@ -6,7 +6,7 @@ var jQT = new $.jQTouch({
 var fn = {
 	ready: function(){
 		document.addEventListener("deviceready", fn.init, false);
-	}
+	},
 	init: function(){
 		$('#acelerometro .individual li').eq(0).tap(ac.iniciar);
 		$('#acelerometro .individual li').eq(1).tap(ac.detener);
