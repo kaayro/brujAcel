8815c7d77fda3379b67a637866bcb9253f93c604
Carlos Celma <solrac@igitsoft.com>
2016-05-07 12:09:36 -0500

    k

--- js/actions.js
+++ js/actions.js
@@ -8,8 +8,12 @@ var fn = {
 		document.addEventListener("deviceready", fn.init, false);
 	},
 	init: function(){
+		//Acelerómetro
 		$('#acelerometro .individual li').eq(0).tap(ac.iniciar);
 		$('#acelerometro .individual li').eq(1).tap(ac.detener);
+		//Brújula
+		$('#brujula .individual li').eq(0).tap(br.iniciar);
+		$('#brujula .individual li').eq(1).tap(br.detener);
 	}
 };
 
--- js/brujula.js
+++ js/brujula.js
@@ -1,3 +1,22 @@
 var br = {
-	
+	watchID: null,
+	iniciar: function(){
+		if(!br.watchID){
+			var options = { frequency: 500 };
+			br.watchID = navigator.compass.watchHeading(br.verDatos, br.error, options);
+		}
+	},
+	detener: function(){
+		if(br.watchID != null){
+			navigator.compass.clearWatch(br.watchID);
+			br.watchID = null;
+			$('#brujula h2').html('Detenido');
+		}
+	},
+	error: function(err){
+		alert(err.code);
+	},
+	verDatos: function(h){
+		$('#brujula h2').html(h.magneticHeading);
+	}
 };
